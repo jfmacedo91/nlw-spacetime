bdb5538751a2b87a7c4edcc4567a5154f6d6de84
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2023-05-18 18:21:31 -0300

    class three completed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
@@ -9,11 +13,17 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, {
+  secret: 'daecd29f8b127ab95db194604245de2d063e8039',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 HTTP server running on http://localhost:3333')
